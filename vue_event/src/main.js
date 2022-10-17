507b736a5c2dc133ff41ef8e3beee41b47cd110e
kukudr <2690404248@qq.com>
2022-10-17 17:18:42 +0800

    17文章管理_发表文章_富文本编辑器引入

--- vue_event/src/main.js
+++ vue_event/src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 import '@/assets/global.less'
 import '@/elementUI/index.js'
+import VueQuillEditor from 'vue-quill-editor'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+Vue.use(VueQuillEditor /* { default global options } */)
+
 Vue.config.productionTip = false
 
 new Vue({ router, store, render: h => h(App) }).$mount('#app')
